984567a5fef07afeca674d2f0858c31400a0d0d7
vladutbutnaru <vlad.butnaru@feel-it-services.com>
2017-01-10 14:08:06 +0200

    Added functionality for language targeting for certain channelSection

---
+++ src/main/java/ro/vladbutnaru/Country.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.vladbutnaru;
+
+import java.util.List;
+
+/**
+ *
+ * @author vlad.butnaru
+ */
+public class Country {
+   public List<String> countries;
+    
+}
